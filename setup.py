f31d8929ba32d0ca94474bfc141b1b6366bba418
Geunseong Jung <gs.jung@impactai.ai>
2023-06-01 09:50:48 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="sugarpowder",
+    version="0.0.1",
+    description="Package of Python syntactic sugars inspired by Elixir, Go, Rust, Julia, and other languages",
+    author="Geunseong Jung",
+    author_email="dreamwayjgs@gmail.com",
+    license="MIT License",
+    packages=["sugarpowder"],
+    zip_safe=False,
+    install_requires=[],
+)
